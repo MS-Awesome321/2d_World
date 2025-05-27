886047d771cc114bda36c876e2c099c476b70a56
MS-Awesome321 <ms9454@princeton.edu>
2025-05-27 14:37:40 -0400

    correct thor.py

--- hardware/thor.py
+++ hardware/thor.py
@@ -1,38 +1,22 @@
-# pip install pylablib
-# pip install --user --upgrade thorlabs-apt-device
-
-# from pylablib.devices import Thorlabs
-# import ft232
-
-# motors = Thorlabs.list_kinesis_devices()
-# print(motors)
-
-# stage = Thorlabs.KinesisMotor(motors[0])
-# stage._move_by(20)
-
-# while stage.is_moving():
-#     print("moving")
-
-# stage.close()
-
-from thorlabs_apt_device.devices.kdc101 import KDC101
-from thorlabs_apt_device.devices.aptdevice import list_devices, APTDevice
-from thorlabs_apt_device.devices.aptdevice_motor import APTDevice_Motor
+from pylablib.devices import Thorlabs
 import time
 
-# print(list_devices())
+# Connect to motor
+stage = Thorlabs.KinesisMotor("27503936")
 
-stage = KDC101(pid='0xfaf0')
-print(stage.status)
-stage.identify()
-# stage.set_jog_params(max_velocity=10, acceleration=10, size=10)
-stage.move_relative(20000)
-time.sleep(1)
-print(stage.status)
+# Get current position (in nanometers)
+pos = stage.get_position()
+print(f"Current position: {pos / 1e6:.3f} mm")
+
+# Move 2 mm down (2,000,000 nm)
+target = pos - 40_000_000
+stage.move_to(target)
+
+# Wait for completion
+while stage.is_moving():
+    time.sleep(0.1)
+
+print(f"Moved to {target / 1e6:.3f} mm")
+
+# Close connection
 stage.close()
-
-# stage.move_jog(direction='forward')
-# print(stage.status)
-# time.sleep(5)
-# stage.stop()
-# stage.close()
