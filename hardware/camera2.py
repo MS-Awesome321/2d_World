2da42ee2d59a33d5803c061e731c533536c3debd
MS-Awesome321 <ms9454@princeton.edu>
2025-06-18 16:59:53 -0400

    pickled array

--- hardware/camera2.py
+++ hardware/camera2.py
@@ -1,13 +1,14 @@
 import subprocess
 import cv2
 import numpy as np
+import pickle
 
 gphoto2 = subprocess.Popen(['gphoto2', '--capture-movie', '--stdout'], stdout=subprocess.PIPE)
 
 buffer = b''
 
 try:
-    while True:
+    for i in range(3):
         buffer += gphoto2.stdout.read(2**18)
 
         start = buffer.find(b'\xff\xd8')
@@ -17,7 +18,14 @@ try:
             jpg = buffer[start:end+2]
             buffer = buffer[end+2:]  # Remove processed bytes
 
-            img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            np_img = np.frombuffer(jpg, dtype=np.uint8)
+            print(np_img)
+            print(np_img.shape)
+            print(np_img.dtype)
+            with open('my_array.pickle', 'wb') as f:
+                # Pickle the array and write it to the file
+                pickle.dump(np_img, f)
+            img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
 
             if img is not None and img.shape == (640, 960, 3):
                 cv2.imshow('Camera Demo', img)
